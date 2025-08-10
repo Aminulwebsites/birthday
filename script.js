f79f85ea3565ad1a89fb7da6e401abcdea1ac436
Aminul Islam <Aminulwebsites@gmail.com>
2025-08-11 01:18:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ var w = c.width = window.innerWidth,
     hh = h / 2,
 
     opts = {
-            strings: ["HAPPY", "BIRTHDAY!", "DEAR"],
+            strings: ["HAPPY", "BIRTHDAY!", "SADIKA KHANAM"],
                     charSize: getResponsiveCharSize(w, h),
                     charSpacing: getResponsiveCharSpacing(w, h),
                     lineHeight: getResponsiveLineHeight(w, h),
